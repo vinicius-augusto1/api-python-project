f041608293c66c6b1ea4c8076ab7fdf4feeaeebc
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-10-29 15:44:09 -0300

    feat: implementação do cálculo do gasto e total de produtos por cliente

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
+from crypt import methods
+
+import pandas as pd
 from flask import Flask, request, jsonify
 import pandas
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+
+
+@app.route('/relatorio-vendas', methods=['POST'])
+def relatorio_vendas():
+    file = request.files['file']
+
+    df = pd.read_csv(file)
+
+    relatorio_cliente = df.groupby(['cliente', 'empresa']).agg(
+        total_gasto = ('valor', lambda x: (x * df['quantidade']).sum()),
+        total_produtos=('produto', 'count')
+    ).reset_index()
+
+    return jsonify(relatorio_cliente.to_dict(orient='records'))
