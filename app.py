4b58c235c8e032b3b666cd8fe7dfd8d35b976b60
Vinícius Augusto <hat.ubuntu@gmail.com>
2024-10-29 15:57:26 -0300

    feat: Adição dos detalhes de produtos comprados por cliente no relatório

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
+def gerar_tabela_vendas(relatorio_cliente):
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.axis('off')
+    ax.axis('tight')
+
+    tabela = ax.table(cellText=relatorio_cliente.values, colLabels=relatorio_cliente.columns, loc='center', cellLoc='center')
+    tabela.auto_set_font_size(False)
+    tabela.set_fontsize(12)
+    tabela.scale(1.2, 1.2)
+
+    plt.show()
 
 @app.route('/relatorio-vendas', methods=['POST'])
 def relatorio_vendas():
